c8d40e4448fe0b0462c90385bddbd83e663f50fa
Chris O'Brien <obrchr96@gmail.com>
2024-06-09 21:35:40 -0700

    Add macro for manually wrapping bindgen methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,27 @@
+#![allow(non_snake_case, dead_code)]
 
+use reaper_low::Reaper as CReaper;
+
+pub struct Reaper(CReaper);
+
+#[macro_export]
+macro_rules! wrap_bindgen {
+    ($reaper:ident, $method:ident, ($($arg:ident: $arg_type:ty),*)) => {
+        impl $reaper {
+            pub fn $method(&self, $($arg: $arg_type),*) {
+                unsafe { (self.0).$method($($arg),*) }
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Reaper;
+    use std::ffi;
+
+    #[test]
+    fn test_wrap_bindgen() {
+        wrap_bindgen!(Reaper, ShowConsoleMsg, (msg: *const ffi::c_char));
+    }
+}
