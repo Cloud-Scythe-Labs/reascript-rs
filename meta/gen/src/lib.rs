786518a3bf0973155910921d368d15dec3a0fb29
Chris O'Brien <obrchr96@gmail.com>
2024-06-10 12:24:32 -0700

    move tests into tests directories

--- meta/gen/src/lib.rs
+++ meta/gen/src/lib.rs
@@ -1,47 +1,13 @@
-#![allow(non_snake_case, dead_code)]
+//! Generative macros.
 
-use reaper_low::Reaper as CReaper;
-
-pub struct Reaper(CReaper);
-
+/// Creates an `impl` block on the wrapper type of methods on the inner type.
 #[macro_export]
 macro_rules! wrap_bindgen {
-    ($reaper:ident, $method:ident, ($($arg:ident: $arg_type:ty),*)) => {
-        impl $reaper {
-            pub fn $method(&self, $($arg: $arg_type),*) {
+    ($wrapper:ident, $method:ident, ($($arg:ident: $arg_type:ty),*), $return_ty:ty) => {
+        impl $wrapper {
+            pub fn $method(&self, $($arg: $arg_type),*) -> $return_ty {
                 unsafe { (self.0).$method($($arg),*) }
             }
         }
     };
-}
-
-#[cfg(test)]
-mod gen_macro_tests {
-    use crate::Reaper;
-    use std::ffi;
-
-    #[test]
-    fn test_wrap_bindgen() {
-        wrap_bindgen!(Reaper, ShowConsoleMsg, (msg: *const ffi::c_char));
-    }
-}
-
-#[cfg(test)]
-mod proc_macro_tests {
-    use reascript_proc::wrap_bindgen;
-
-    pub struct Inner;
-
-    #[wrap_bindgen]
-    impl Inner {
-        pub unsafe fn is_true(&self) -> bool {
-            true
-        }
-    }
-
-    #[test]
-    fn test_wrap_bindgen() {
-        let reaper = REAPER(Inner);
-        assert!(reaper.is_true());
-    }
 }
---
+++ meta/gen/tests/wrap_bindgen.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+
+use reascript_gen::wrap_bindgen;
+
+struct Inner;
+
+#[allow(dead_code)]
+impl Inner {
+    pub unsafe fn unit(&self) {
+        ()
+    }
+    pub unsafe fn no_args_with_return(&self) -> bool {
+        true
+    }
+    pub unsafe fn with_args(&self, arg1: bool, arg2: u32) -> (bool, u32) {
+        (arg1, arg2)
+    }
+}
+
+struct Outer(Inner);
+
+#[test]
+fn test_wrap_bindgen() {
+    wrap_bindgen!(Outer, no_args_with_return, (), bool);
+    wrap_bindgen!(Outer, with_args, (arg1: bool, arg2: u32), (bool, u32));
+    wrap_bindgen!(Outer, unit, (), ());
+
+    let outer = Outer(Inner);
+    assert!(outer.is_true());
+    assert_eq!(outer.with_args(true, 0), (true, 0));
+    assert_eq!(outer.unit(), ());
+}
