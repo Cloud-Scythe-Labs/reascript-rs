7d95697173f12a3a6fbf4c7a98cdf3a54775ae31
Chris O'Brien <obrchr96@gmail.com>
2024-06-10 18:30:39 -0700

    pass arg idents to inner method call, fix imports

--- meta/proc/src/lib.rs
+++ meta/proc/src/lib.rs
@@ -1,6 +1,11 @@
+//! Procedural macros.
+
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Attribute, Ident, ImplItem, ItemImpl, Type, TypePath};
+use syn::{
+    parse_macro_input, Attribute, FnArg, Ident, ImplItem, ImplItemFn, ItemImpl, Pat, Signature,
+    Type, TypePath,
+};
 
 /// Not sure if this is necessary yet, but we may want to include certain methods.
 /// This would be the attribute ident added to each included function `#[wrap_include]`.
@@ -14,6 +19,9 @@ const _WRAP_EXCLUDE_IDENT: &str = "wrap_exclude";
 /// it removes the need to constantly use the `unsafe` keyword for Reaper methods.
 /// It also an important piece of the puzzle that gets us closer to a fully automated,
 /// self releasing crate.
+///
+/// NOTE: This is a WIP, and may not cover all cases, e.g. this assumes the methods are
+/// `unsafe` and does not check before wrapping the inner method.
 #[proc_macro_attribute]
 pub fn wrap_bindgen(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let impl_block = parse_macro_input!(item as ItemImpl);
@@ -33,11 +41,11 @@ pub fn wrap_bindgen(_attr: TokenStream, item: TokenStream) -> TokenStream {
 }
 
 /// Filter methods with a specified `#[attribute_path]`
-fn extract_methods(impl_block: &ItemImpl) -> Vec<syn::ImplItemFn> {
+fn extract_methods(impl_block: &ItemImpl) -> Vec<ImplItemFn> {
     impl_block
         .items
         .iter()
-        .filter_map(|item| -> Option<syn::ImplItemFn> {
+        .filter_map(|item| -> Option<ImplItemFn> {
             if let ImplItem::Fn(method) = item {
                 // if method.attrs.iter().any(|attr| is_ident(attr, ident)) {
                 return Some(method.clone());
@@ -56,19 +64,28 @@ fn _is_ident(attr: &Attribute, ident: &str) -> bool {
 
 // TODO: figure out how to make an ident so this can be more dynamic
 /// Generate the wrapper and impl methods for the wrapper.
-fn generate_wrapper_impl(
-    methods: &[syn::ImplItemFn],
-    input_ident: &Ident,
-) -> proc_macro2::TokenStream {
+fn generate_wrapper_impl(methods: &[ImplItemFn], input_ident: &Ident) -> proc_macro2::TokenStream {
     let methods = methods.iter().map(|method| {
-        let method_name = &method.sig.ident;
-        let parameters = &method.sig.inputs;
-        let return_type = &method.sig.output;
+        let sig: &Signature = &method.sig;
+        let method_name = &sig.ident;
+        let parameters = &sig.inputs;
+        let return_type = &sig.output;
+        let args = parameters.iter().filter_map(|fn_arg| -> Option<Ident> {
+            if let FnArg::Typed(pat_type) = &fn_arg {
+                if let Pat::Ident(pat_ident) = *pat_type.pat.clone() {
+                    Some(pat_ident.ident)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        });
 
         quote! {
             pub fn #method_name(#parameters) #return_type {
                 unsafe {
-                    (self.0).#method_name(#parameters)
+                    (self.0).#method_name(#(#args),*)
                 }
             }
         }
