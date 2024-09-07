f75583f1c321753df722e7db1b0a73c25301665b
andrewvious <ohbandrew@gmail.com>
2024-09-07 11:55:22 -0500

    modify imports for update.sh

--- lib/update.nix
+++ lib/update.nix
@@ -1,8 +1,16 @@
-{ pkgs, ... }:
-pkgs.writeShellScriptBin "update.sh" ''
+{ writeShellScriptBin, coreutils, curl, gnugrep, gawk }:
+let
+  echo = "${coreutils}/bin/echo";
+  pwd = "${coreutils}/bin/pwd";
+  mv = "${coreutils}/bin/mv";
+  curl' = "${curl}/bin/curl";
+  grep' = "${gnugrep}/bin/grep";
+  awk' = "${gawk}/bin/awk";
+in
+writeShellScriptBin "update.sh" ''
   set -euo pipefail
 
-  latest_version=$(curl -s https://www.reaper.fm/download.php | grep -oP 'REAPER \K[0-9.]+')
+  latest_version=$(${curl'} -s https://www.reaper.fm/download.php | ${grep'} -oP 'REAPER \K[0-9.]+')
 
   function update_nix_flake() {
     local flake="$1"
@@ -12,9 +20,9 @@ pkgs.writeShellScriptBin "update.sh" ''
     reap_tar="reaper''${reap_tag}_linux_x86_64.tar.xz"
     reaper_tarball_link="https://www.reaper.fm/files/''${file_tag}.x/''${reap_tar}"
 
-    echo "Updating REAPER tarball URL to: ''${reaper_tarball_link}"
+   ${echo} "Updating REAPER tarball URL to: ''${reaper_tarball_link}"
 
-    awk -v new_url="$reaper_tarball_link" '
+    ${awk'} -v new_url="$reaper_tarball_link" '
     BEGIN { url_updated = 0 }
     {
       if ($0 ~ /reaper\s*=\s*\{/ && url_updated == 0) {
@@ -33,13 +41,13 @@ pkgs.writeShellScriptBin "update.sh" ''
         print
     }
   }
-  ' "$flake" > "''${flake}.tmp" && mv "''${flake}.tmp" "$flake"
+  ' "$flake" > "''${flake}.tmp" && ${mv} "''${flake}.tmp" "$flake"
   }
 
-  nix_flake="$(pwd)/flake.nix"
+  nix_flake="$(${pwd})/flake.nix"
 
   if [ ! -f "$nix_flake" ]; then
-    echo "Error: $nix_flake does not exist."
+    ${echo} "Error: $nix_flake does not exist."
     exit 1
   fi
 
