5bfe856f359140405974259ea0dd13497e714c0c
andrewvious <106849954+andrewvious@users.noreply.github.com>
2024-09-08 09:31:01 -0500

    Add script for updating Reaper packages (#20)
    Closes #19 
    
    Adds `update_reaper_flake_input.sh` which updates the Reaper packages to
    the **most** recent version and runs a check in CI that opens a pull
    request that will update the Reaper input to the flake.
    
    ---------
    
    Co-authored-by: [eureka@nixos] <57543709+eureka-cpu@users.noreply.github.com>
    Co-authored-by: Cloud Scythe Labs <cloudscythelabs@gmail.com>

--- flake.nix
+++ flake.nix
@@ -111,6 +111,7 @@
             devShells.default = craneLib.devShell {
               checks = self.checks.${system};
               packages = with pkgs; [
+                (pkgs.callPackage ./scripts/update_reaper_flake_input.nix { })
                 nil
                 nixpkgs-fmt
               ] ++ rustToolchain.complete;
