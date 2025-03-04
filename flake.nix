c8d11b341c08dad404a3f979da9f70d1c7f6fd0c
Cloud Scythe Labs <cloudscythelabs@gmail.com>
2025-03-04 01:37:52 +0000

    Update Reaper tarball URL 7.34

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     reaper = {
-      url = "https://www.reaper.fm/files/7.x/reaper733_linux_x86_64.tar.xz";
+      url = "https://www.reaper.fm/files/7.x/reaper734_linux_x86_64.tar.xz";
       flake = false;
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
