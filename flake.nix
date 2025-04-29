36865177ae4b35701a42e899e09989e1549ac42a
[eureka@nixos] <57543709+eureka-cpu@users.noreply.github.com>
2025-04-29 00:41:33 -0500

    Update Reaper tarball URL 7.37 (#77)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     reaper = {
-      url = "https://www.reaper.fm/files/7.x/reaper736_linux_x86_64.tar.xz";
+      url = "https://www.reaper.fm/files/7.x/reaper737_linux_x86_64.tar.xz";
       flake = false;
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
